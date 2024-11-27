985ef719f732b94eb84e66904283a128b6aa08db
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-11-27 21:20:53 +0530

    beta v0.14 27112024

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -35,6 +35,6 @@ export async function GET(req: NextRequest) {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "http://localhost:3000/"
+      : "https://b-portfolio-psi.vercel.app/"
   );
 }
